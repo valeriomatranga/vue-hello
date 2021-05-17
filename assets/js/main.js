ad8fc1a461e0e591d2bd4aec4a3010c80bd15645
valeriomatranga <matrangav@gmail.com>
2021-05-17 16:32:18 +0200

    vue.js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const app = new Vue({
+    el:'#app',
+    el:'#messaggio',
+
+    data:{
+        msg:'Benvenuto Vue.js',
+        img:'./assets/img/sfondo.png'
+    },
+})
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
